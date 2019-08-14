1fa12d4d12303b0c287f8496e6cfc8b8d9e2a7ca
wildbrother <wildbrother@yandex.ru>
2019-08-14 21:59:28 +0600

    Созданы компоненты отображения страницы, настроен webpack

---
+++ src/components/sort.js
@@ -0,0 +1,8 @@
+export const getSortMarkup = function () {
+  return `
+        <ul class="sort">
+    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+    <li><a href="#" class="sort__button">Sort by date</a></li>
+    <li><a href="#" class="sort__button">Sort by rating</a></li>
+  </ul>`;
+};

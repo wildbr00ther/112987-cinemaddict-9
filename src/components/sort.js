07b4fc1ae4e8d75b6505f8f6b6cad9dc74028b55
wildbrother <wildbrother@yandex.ru>
2019-09-15 15:34:34 +0600

    Добавлена сортировка фильмов

--- src/components/sort.js
+++ src/components/sort.js
@@ -3,9 +3,9 @@ import {AbstractComponent} from './absctract-component';
 export class Sort extends AbstractComponent {
   getTemplate() {
     return `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
+      <li><a href="#" data-sort-type="default" class="sort__button sort__button--active">Sort by default</a></li>
+      <li><a href="#" data-sort-type="date" class="sort__button">Sort by date</a></li>
+      <li><a href="#" data-sort-type="rating" class="sort__button">Sort by rating</a></li>
     </ul>`;
   }
 }
